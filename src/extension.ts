3b7ce0cb9ef94e7cb87b7f8f57cff385a05519bd
Chaitanya Shahare <shaharechaitanya3@gmail.com>
2024-04-28 20:34:07 +0530

    feat: add database-connector icon to activity bar

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,31 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  console.log('Your extension "database-connecter" is now active!');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "database-connecter" is now active!');
+  const treeDataProvider = new DatabaseExplorerProvider();
+  vscode.window.registerTreeDataProvider("databaseView", treeDataProvider);
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('database-connecter.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from database-connector!');
-	});
+  let disposable = vscode.commands.registerCommand(
+    "database-connecter.helloWorld",
+    () => {
+      vscode.window.showInformationMessage(
+        "Hello from your Database Connector!"
+      );
+    }
+  );
 
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
+class DatabaseExplorerProvider implements vscode.TreeDataProvider<string> {
+  getTreeItem(element: string): vscode.TreeItem {
+    return new vscode.TreeItem(element);
+  }
+
+  getChildren(element?: string): Thenable<string[]> {
+    return Promise.resolve(["MongoDB", "PostgreSQL", "MySQL"]);
+  }
+}
+
 export function deactivate() {}
